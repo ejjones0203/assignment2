728fb14b245ceae6fc56fa31b5db12ab94b4a14d
ejjones0203 <enjiejones03@gmail.com>
2025-02-05 21:36:54 -0500

    Update assignment2.py

--- assignment2.py
+++ assignment2.py
@@ -11,22 +11,22 @@ from operator import itemgetter
 
 def sort (file):
 
-    students = []
+    students = [] #initialize an empty list to store students
 
-    inputfile = open(file, "r")
-    for line in inputfile:
-        word = line.split()
-        names = word[0]
-        grade = []
-        for x in word[1:]:
-            grade.append(int(x))
-        avg = sum(grade) / len(grade)
-        students.append((names, avg))
+    inputfile = open(file, "r") #standard file open procedure
+    for line in inputfile: #iterates through each line of the input file
+        word = line.split() #split each word at space
+        names = word[0] #take the first item in list and store it as a name
+        grade = [] #initialize an empty list to store grades
+        for x in word[1:]: #slice the list from the names
+            grade.append(int(x)) #append grades from file
+            avg = sum(grade) / len(grade) #average the grades
+        students.append((names, avg)) #combine both name and grades
 
-    students.sort(key=itemgetter(1), reverse = True)
+    students.sort(key=itemgetter(1), reverse = True) #itemgetter is pointing to the second item to sort, also appending reverse or the list wont appear in correct order
 
     for names, avg in students:
-        print(f"{names} {avg:.2f}")
+        print(f"{names} {avg:.2f}") #iterate and print all names and averages using the elusive f string
 
 file = "assignment2input.txt"
 
